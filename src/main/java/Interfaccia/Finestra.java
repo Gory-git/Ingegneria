9319a9f21b7449cd9f6c4df04bf699076de1e27b
Gory-git <61806971+Gory-git@users.noreply.github.com>
2024-05-23 17:55:34 +0200

    prime finestre del gioco

--- src/main/java/Interfaccia/Finestra.java
+++ src/main/java/Interfaccia/Finestra.java
@@ -2,8 +2,7 @@ package Interfaccia;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Finestra
 {
@@ -21,50 +20,70 @@ public class Finestra
     private void finestraIniziale()
     {
         JPanel panelIniziale = new JPanel();
-        panelIniziale.setBounds(50, 50, 400, 400);
+        panelIniziale.setLayout(new FlowLayout());
+        panelIniziale.setBackground(Color.LIGHT_GRAY);
 
         JButton nuovoGioco = new JButton("Nuovo Gioco");
-        nuovoGioco.setBounds(200, 180, 120, 40);
+        //nuovoGioco.setBounds(150, 200, 120, 40);
         nuovoGioco.addActionListener(new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
+                frame.remove(panelIniziale);
                 finestraNuovoGioco();
             }
         });
 
         JButton caricaGioco = new JButton("Carica Gioco");
-        caricaGioco.setBounds(200, 240, 120, 40);
+        //caricaGioco.setBounds(200, 200, 120, 40);
         caricaGioco.addActionListener(new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
+                frame.remove(panelIniziale);
                 finestraNuovoGioco();
             }
         });
 
-        frame.add(panelIniziale);
         panelIniziale.add(nuovoGioco);
         panelIniziale.add(caricaGioco);
 
-        frame.setLayout(null);
+        frame.add(panelIniziale);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
     private void finestraNuovoGioco()
     {
         //frame.setVisible(false);
-        frame.removeAll();
 
         JPanel panelNuovoGioco = new JPanel();
-        Button avviaGioco = new Button("Avvia!");
+        panelNuovoGioco.setLayout(new FlowLayout());
+        panelNuovoGioco.setBackground(Color.LIGHT_GRAY);
+        JButton avviaGioco = new JButton("Avvia!");
         avviaGioco.setBounds(350, 400, 120, 40);
         panelNuovoGioco.add(avviaGioco);
-        panelNuovoGioco.setVisible(true);
-        //frame.setLayout(null);
-        //frame.setVisible(true);
+
+
+        JSlider sliderDimensdione = new JSlider(JSlider.HORIZONTAL, 3, 9, 5);
+        sliderDimensdione.setMajorTickSpacing(1);
+        sliderDimensdione.setPaintTicks(true);
+        sliderDimensdione.setPaintLabels(true);
+
+        panelNuovoGioco.add(sliderDimensdione);
+
+        SpinnerModel valori = new SpinnerNumberModel(1, 0, 100, 1);
+        JSpinner spinnerSoluzioni = new JSpinner(valori);
+
+        // TODO estrapolare i dati dagli oggetti sopra e avviare il gioco.
+
+        panelNuovoGioco.add(spinnerSoluzioni);
+
+        frame.add(panelNuovoGioco);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
     }
 }
